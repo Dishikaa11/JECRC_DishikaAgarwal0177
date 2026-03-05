25ba4c61fdcab3e4100d79e1b392387921f3e7d0
Dishika <dishikaa11@gmail.com>
2026-03-05 13:56:35 +0530

    Capgemini training work

---
+++ 17_feb_26/Even_Odd/Even_Odd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static bool CheckEvenOdd(int a, int b)
+    {
+        if ((a % 2 == 0 && b % 2 == 0) ||
+            (a % 2 != 0 && b % 2 != 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    static void Main()
+    {
+        Console.WriteLine("Enter first number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        bool result = CheckEvenOdd(num1, num2);
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
