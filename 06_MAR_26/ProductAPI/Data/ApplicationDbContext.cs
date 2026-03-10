50eda231adb2620c98569bccfa6954e4e9868c70
Dishika <dishikaa11@gmail.com>
2026-03-10 10:16:55 +0530

    09_03_26

---
+++ 06_MAR_26/ProductAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+namespace ProductAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                 modelBuilder.Entity<Product>()
+                    .Property(p => p.Price)
+                     .HasPrecision(18,2);
+            }
+    }
+}
+
